a18b8c877c1ffc99d4218ed14a36cff983626c86
github username <github email address>
2018-09-18 21:38:16 -0400

    donegit add .

--- Javascript/Katas_7-8/HelpSuzukiRake/index.js
+++ Javascript/Katas_7-8/HelpSuzukiRake/index.js
@@ -2,10 +2,20 @@
 
 var garden2 = 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel'
 
-function rakeGarden(garden) {
-  var rakedGarden;
-  // TODO: Program Me
-  return rakedGarden;
-}
+var garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+//garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
 
-console.log(rakeGarder(garden2))
+function rakeGarden(garden) {
+  return garden.split(' ').map(
+   function(el) {
+     if (el !== 'gravel' && el !== 'rock'){
+       return 'gravel'
+     }
+     else {
+       return el
+     }
+   }).join(' ')
+}
+//refine
+
+console.log(rakeGarden(garden))
--- Javascript/Katas_7-8/HelpSuzukiRake/solution.js
+++ Javascript/Katas_7-8/HelpSuzukiRake/solution.js
@@ -1,1 +1,7 @@
 //write only the solution function here
+function rakeGarden(garden) {
+  return garden
+    .split(' ')
+    .map(item => item === 'rock'? 'rock': 'gravel')
+   .join(' ')
+}
